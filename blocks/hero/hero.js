316e5a32b43404b9ea1d7cd3939f0629e90de85b
SudarsanGouda-Adobe <inf16883@adobe.com>
2026-09-10 07:57:13 +0530

    hero block  added

---
+++ blocks/hero/hero.js
@@ -0,0 +1,8 @@
+export default async function decorate(block) {
+    // const h1 = document.querySelector('h1');
+    // const p = document.querySelector(p);
+    // const layout = document.createElement('div');
+    // layout.append(p,h1);
+    // block.append(layout);
+    
+}
