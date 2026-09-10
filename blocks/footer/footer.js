b446d70f848468b479c23ddfeddd4b186cd16088
SudarsanGouda-Adobe <inf16883@adobe.com>
2026-09-10 18:09:37 +0530

    footer css

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,5 +16,28 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  // footer nav
+  const wrapper = footer.querySelector('.default-content-wrapper');
+
+if (wrapper) {
+  const logo = wrapper.querySelector('picture')?.closest('p');
+  const tagline = logo?.nextElementSibling;
+  const address = tagline?.nextElementSibling;
+  const navList = wrapper.querySelector('ul');
+
+  if (logo && tagline && address && navList) {
+    const brand = document.createElement('div');
+    brand.className = 'footer-brand';
+
+    const nav = document.createElement('div');
+    nav.className = 'footer-nav';
+
+    brand.append(logo, tagline, address);
+    nav.append(navList);
+
+    wrapper.replaceChildren(brand, nav);
+  }
+}
+
   block.append(footer);
 }
